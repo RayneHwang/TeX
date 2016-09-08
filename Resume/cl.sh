329d67274387c68734af0fd6c78bd8ccb546e377
lei,HUANG <mrsatangel@gmail.com>
2016-09-08 21:04:42 +0800

    add Resume

---
+++ Resume/cl.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+rm -f ./*.aux*
+rm -f ./*.bak*
+rm -f ./*.log*
+rm -f ./*.bbl*
+rm -f ./*.blg*
+rm -f ./*.thm*
+rm -f ./*.toc*
+rm -f ./*.out*
+rm -f ./*.fdb_latexmk
+rm -f ./*.synctex.gz
---
+++ Resume/cv-sections/cl.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+rm -f ./*.aux*
+rm -f ./*.bak*
+rm -f ./*.log*
+rm -f ./*.bbl*
+rm -f ./*.blg*
+rm -f ./*.thm*
+rm -f ./*.toc*
+rm -f ./*.out*
